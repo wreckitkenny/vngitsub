989311bb741ef337da83e9a52ea4afff97f9265a
Wreck-it Kenny <tungtt4@vnpay.vn>
2023-07-14 15:10:05 +0700

    ## [1.0.3] | 2023-07-14 ### Added/Removed - Added log info for old tag getting process ### Changed - Changed Dockerfile building in multi-stages - Changed Telegram format ### Fixed

--- main.go
+++ main.go
@@ -1,28 +1,26 @@
 package main
 
 import (
+	"os"
 	"vngitSub/pkg/controller"
 	"vngitSub/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
-const version string = "1.0.0"
-
 func main(){
-	//Load Default Config
-	// defaultConfig, err := utils.LoadConfig("config")
 	logger := utils.ConfigZap()
-	// if err != nil {
-	// 	logger.Errorf("Loading configuration...failed: %s", err)
-	// } else {
-	// 	logger.Debug("Loading configuration...ok")
-	// }
 
 	//Configure GIN
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 	r.SetTrustedProxies(nil)
-	logger.Infof("Loading version...%s", version)
+
+	version, err := os.ReadFile("VERSION")
+	if err != nil {
+		logger.Errorf("Loading version...FAILED: %s", err)
+	} else {
+		logger.Infof("Loading version...%s", version)
+	}
 	controller.MsgHandler()
 }
